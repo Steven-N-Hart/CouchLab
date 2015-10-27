02a24b39aeb324404c1656814f9a8f0228ce863a
Raymond <raymond.moore301@gmail.com>
2015-10-27 12:37:06 -0500

    chaning tactics, go away from nokogiri, persue the watir version instead for IDT oligoanalyzer

--- Carriers/PrimerPilot/webby_PrimerChecking.rb
+++ Carriers/PrimerPilot/webby_PrimerChecking.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'mechanize'
 require 'logger'
 require 'pp'
+require 'watir'
 
 
 
@@ -29,12 +30,71 @@ end
 
 base="https://www.idtdna.com/calc/analyzer"
 
+
+
+browser = Watir::Browser.new
+browser.goto base
+
+
+myQuery = Hash.new
+lp=true
+
+browser.div(:class => "textAreaStyle").textarea.set "ctctctatgggcagtcggtgattTATAAATACTGCAGTATAAAATAATTAT"
+browser.button(:text => 'Analyze').click
+
+
+##
+i=0
+while lp==true
+  sleep 1
+  if ! browser.div(:id => "OAResults").nil?
+      lp=false
+  end
+  i+=1
+  if i > 120   ## approx 2 mins.
+    $stderr.puts "Unable to reach results!! Quit."
+    exit
+  end
+end
+
+targetTbl = browser.div(:id => "OAResults").table
+
+##pp targetTbl
+
+targetTbl.rows.each do |r|
+  c = r.cells
+  myQuery[ c[0].text ] = c[1].text
+end
+
+
+pp myQuery
+
+
+
+
+exit
+
+
+
 agent = Mechanize.new { |a| a.log = Logger.new("primer_check.log") }
 
-idtForm = agent.get(base)
+pageIDT = agent.get(base)
 
-pp idtForm.forms
+queryIDT = pageIDT.at '#seqInput'
+queryIDT.set_attribute  'value', "ctctctatgggcagtcggtgattTATAAATACTGCAGTATAAAATAATTAT"
 
+
+rezIDT = agent.click( pageIDT.search('button').select{|b| b.text == "Analyze"}[0] )
+
+anaTable = rezIDT.search("div div.row div.col-md-12 div.panel.panel-default table.table").first
+
+#pageIDT.search('button').select{|b| b.text == "Analyze"}
+#pp idtForm.forms
+
+
+pg2 = agent.get("https://www.idtdna.com/calc/analyzer#OAResults")
+
+div#OAResults.tab-pane.active
 
 
 
