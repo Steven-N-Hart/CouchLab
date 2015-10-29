88bb1ad27a671543d966de1ac4ad82608281dae8
Raymond <raymond.moore301@gmail.com>
2015-10-29 11:41:21 -0500

    just has IDT oligoanalyzer, primer3 is not in this script

--- Carriers/PrimerPilot/webby_PrimerChecking.rb
+++ Carriers/PrimerPilot/webby_PrimerChecking.rb
@@ -27,80 +27,88 @@ if !opt.has_key?('-f')
   exit 1
 end
 
+inputFile=opt['-f']
+outputFile= inputFile.sub('seq.tsv', 'check.tsv')
+output = File.open( outputFile,"w" )
+#outHeader=[]
+
 
 base="https://www.idtdna.com/calc/analyzer"
-
-
-
 browser = Watir::Browser.new
 browser.goto base
 
+File.foreach(inputFile).with_index do |line, line_num|
+  #puts "#{line_num}: #{line}"
+  line.chomp!
 
-myQuery = Hash.new
-lp=true
+  next if line_num == 0
+  #if line_num > 1
+  #  break
+  #end
 
-browser.div(:class => "textAreaStyle").textarea.set "ctctctatgggcagtcggtgattTATAAATACTGCAGTATAAAATAATTAT"
-browser.button(:text => 'Analyze').click
+  row = line.split(/\t/)
+
+  adpt = /^[[:lower:]]+/.match(row[1])[0]
+  pNom = row[0].sub(/_(F|R)(\d+)$/, '_S\2')
+  rHash = {"PrimerName"=>row[0],"Pair"=>pNom,"Seq"=>row[1],"MateSeq"=>row[2],"Adapter"=>adpt,"NumAmps"=>row[3],
+            "Location"=>row[4],"Size"=>row[5].sub('hp','').to_i,"ProductSeq"=>row[6]}
 
 
-##
-i=0
-while lp==true
-  sleep 1
-  if ! browser.div(:id => "OAResults").nil?
+
+  #myQuery = Hash.new
+  lp=true
+  browser.div(:class => "textAreaStyle").textarea.set rHash["Seq"]      #"ctctctatgggcagtcggtgattTATAAATACTGCAGTATAAAATAATTAT"
+  browser.button(:text => 'Analyze').click
+
+
+## Loop to wait for results to return from server.
+  i=0
+  while lp==true
+    sleep 1
+    if ! browser.div(:id => "OAResults").nil?
       lp=false
+    end
+    i+=1
+    if i > 120   ## approx 2 mins.
+      $stderr.puts "Unable to reach results!! Quit."
+      exit
+    end
   end
-  i+=1
-  if i > 120   ## approx 2 mins.
-    $stderr.puts "Unable to reach results!! Quit."
-    exit
-  end
-end
 
-targetTbl = browser.div(:id => "OAResults").table
+  targetTbl = browser.div(:id => "OAResults").table
 
 ##pp targetTbl
 
-targetTbl.rows.each do |r|
-  c = r.cells
-  myQuery[ c[0].text ] = c[1].text
+  targetTbl.rows.each do |r|
+    c = r.cells
+#    myQuery[ c[0].text ] = c[1].text
+    rHash[ c[0].text ] = c[1].text
+  end
+
+
+  #pp myQuery
+
+
+
+  sleep rand(5) ## Don't hit server too hard. wait between 1-5 secs.
+
+
+  if line_num == 1
+    output.puts rHash.map{|k,v| "#{k}"}.join("\t")
+  end
+
+  output.puts rHash.map{|k,v| "#{v}"}.join("\t")
+
+  #pp rHash
+
+
+
 end
 
 
-pp myQuery
 
 
 
 
-exit
 
 
-
-agent = Mechanize.new { |a| a.log = Logger.new("primer_check.log") }
-
-pageIDT = agent.get(base)
-
-queryIDT = pageIDT.at '#seqInput'
-queryIDT.set_attribute  'value', "ctctctatgggcagtcggtgattTATAAATACTGCAGTATAAAATAATTAT"
-
-
-rezIDT = agent.click( pageIDT.search('button').select{|b| b.text == "Analyze"}[0] )
-
-anaTable = rezIDT.search("div div.row div.col-md-12 div.panel.panel-default table.table").first
-
-#pageIDT.search('button').select{|b| b.text == "Analyze"}
-#pp idtForm.forms
-
-
-pg2 = agent.get("https://www.idtdna.com/calc/analyzer#OAResults")
-
-div#OAResults.tab-pane.active
-
-
-
-sleep rand(5) ## Don't hit server too hard. wait between 1-5 secs.
-
-
-
-#             input
-#id="seqInput"
